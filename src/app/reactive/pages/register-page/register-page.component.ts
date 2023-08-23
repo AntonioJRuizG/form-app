65beaf5c23980758caa96a0ccf2b4a4e65d49e9c
Antonio Ruiz <antonioruizgarcia90@gmail.com>
2023-08-24 00:19:14 +0200

    add register form builder

--- src/app/reactive/pages/register-page/register-page.component.ts
+++ src/app/reactive/pages/register-page/register-page.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
   styleUrls: [],
 })
-export class RegisterPageComponent {}
+export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
+
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO obtener validacion desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
+}
